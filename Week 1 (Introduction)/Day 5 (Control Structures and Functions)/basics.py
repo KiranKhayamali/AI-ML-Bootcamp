91ac1db8925713bb86b44025d82dc892670f2bbd
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-08 19:40:40 +0545

    Task 2

--- Week 1 (Introduction)/Day 5 (Control Structures and Functions)/basics.py
+++ Week 1 (Introduction)/Day 5 (Control Structures and Functions)/basics.py
@@ -29,22 +29,51 @@ Conditional Statement
 
 #Task 2
 #using dictionary and loop
-score = 0
-question = {
-    "What is captial of Nepal?": "kathmandu",
-    "What is the national bird of Nepal?" : "daphe",
-    "What is the tallest mountain of the world?": "everest",
-    "What is the deepest valley of the world?": "arun valley",
-    "Who is the prime minister of nepal?": "kp oli"
-}
+# score = 0
+# question = {
+#     "What is captial of Nepal?": "kathmandu",
+#     "What is the national bird of Nepal?" : "daphe",
+#     "What is the tallest mountain of the world?": "everest",
+#     "What is the deepest valley of the world?": "arun valley",
+#     "Who is the prime minister of nepal?": "kp oli"
+# }
 
-for ques, ans in question.items():
-    print(ques)
-    answer = input("Enter your answer:")
-    if (answer.lower()==ans):
+# for ques, ans in question.items():
+#     print(ques)
+#     answer = input("Enter your answer:")
+#     if (answer.lower()==ans):
+#         score +=1
+#     else:
+#         print(f"Game Over! {score} of 5")
+#         break
+
+# print(f"Game Over! {score} of 5")
+
+#using nested loop
+score = 0
+print("What is captial of Nepal?")
+if (input("Answer:").lower() == "kathmandu"):
+    score +=1
+    print("What is the national bird of Nepal?")
+    if (input("Answer:").lower() == "daphe"):
         score +=1
+        print("What is the tallest mountain of the world?")
+        if (input("Answer:").lower() == "everest"):
+            score +=1
+            print("What is the deepest valley of the world?")
+            if (input("Answer:").lower() == "arun valley"):
+                score +=1
+                print("Who is the prime minister of nepal?")
+                if (input("Answer:").lower() == "kp oli"):
+                    score +=1
+                    print(f"Game Over! {score} of 5")
+                else:
+                    print(f"Game Over! {score} of 5")
+            else:
+                print(f"Game Over! {score} of 5")
+        else:
+            print(f"Game Over! {score} of 5")
     else:
         print(f"Game Over! {score} of 5")
-        break
-
-print(f"Game Over! {score} of 5")
+else:
+    print(f"Game Over! {score} of 5")

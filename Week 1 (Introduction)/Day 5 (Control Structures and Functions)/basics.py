e9cfe8b89aa5e1f44c58b85f17589ab91d76fcb0
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-08 19:12:34 +0545

    Task 1

--- Week 1 (Introduction)/Day 5 (Control Structures and Functions)/basics.py
+++ Week 1 (Introduction)/Day 5 (Control Structures and Functions)/basics.py
@@ -10,10 +10,18 @@ Conditional Statement
 
 '''
 
-x = 9
-if (x==0):
-    print(f"{x} is zero number.")
-elif (x%2==0):
-    print(f"{x} is an even number.")
+# x = 9
+# if (x==0):
+#     print(f"{x} is zero number.")
+# elif (x%2==0):
+#     print(f"{x} is an even number.")
+# else:
+#     print(f"{x} is an odd number.")
+
+username = input("Enter Username:")
+pasword = input("Enter Password:")
+
+if (username.lower() == "kiran") and (pasword == "password"):
+    print(f"Welcome {username}!")
 else:
-    print(f"{x} is an odd number.")
+    print("Call 911")

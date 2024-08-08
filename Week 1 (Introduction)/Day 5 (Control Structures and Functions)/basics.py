f24f34d9c5c5feac7ec5035c6dd33e0d5dcdd166
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-08 19:31:24 +0545

    Task 2

--- Week 1 (Introduction)/Day 5 (Control Structures and Functions)/basics.py
+++ Week 1 (Introduction)/Day 5 (Control Structures and Functions)/basics.py
@@ -18,10 +18,33 @@ Conditional Statement
 # else:
 #     print(f"{x} is an odd number.")
 
-username = input("Enter Username:")
-pasword = input("Enter Password:")
+#Task 1
+# username = input("Enter Username:")
+# pasword = input("Enter Password:")
 
-if (username.lower() == "kiran") and (pasword == "password"):
-    print(f"Welcome {username}!")
-else:
-    print("Call 911")
+# if (username.lower() == "kiran") and (pasword == "password"):
+#     print(f"Welcome {username}!")
+# else:
+#     print("Call 911")
+
+#Task 2
+#using dictionary and loop
+score = 0
+question = {
+    "What is captial of Nepal?": "kathmandu",
+    "What is the national bird of Nepal?" : "daphe",
+    "What is the tallest mountain of the world?": "everest",
+    "What is the deepest valley of the world?": "arun valley",
+    "Who is the prime minister of nepal?": "kp oli"
+}
+
+for ques, ans in question.items():
+    print(ques)
+    answer = input("Enter your answer:")
+    if (answer.lower()==ans):
+        score +=1
+    else:
+        print(f"Game Over! {score} of 5")
+        break
+
+print(f"Game Over! {score} of 5")

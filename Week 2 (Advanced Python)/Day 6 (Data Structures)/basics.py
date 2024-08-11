70d3e8cd6e7c956ea92846016055e354e2656f4d
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-11 19:26:55 +0545

    iterating over list

--- Week 2 (Advanced Python)/Day 6 (Data Structures)/basics.py
+++ Week 2 (Advanced Python)/Day 6 (Data Structures)/basics.py
@@ -4,3 +4,18 @@ list_var = [1, "kiran", 1.3]
 print(list_var, type(list_var))
 print("**" * 30)
 
+#interating over list using range
+for i in range(0, len(list_var)):
+    print(list_var[i])
+
+#iterating over list directly using element
+print("**" * 30)
+for list_element in list_var:
+    print(list_element)
+
+#iterating over list using enumerate using both index and element
+print("**" * 30)
+for list_index, list_element in enumerate(list_var):
+    print(f"{list_element} is at {list_index}")
+
+print("**" * 30)

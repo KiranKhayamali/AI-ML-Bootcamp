7207f2ca5115f121c6ce15183ef3a58cfc3b06dd
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-11 19:43:54 +0545

    slicing the list

--- Week 2 (Advanced Python)/Day 6 (Data Structures)/basics.py
+++ Week 2 (Advanced Python)/Day 6 (Data Structures)/basics.py
@@ -1,53 +1,58 @@
-#List
-list_var = [1, "kiran", 1.3]
+# #List
+# list_var = [1, "kiran", 1.3]
 
-print(list_var, type(list_var))
-print("**" * 30)
+# print(list_var, type(list_var))
+# print("**" * 30)
 
-#interating over list using range
-for i in range(0, len(list_var)):
-    print(list_var[i])
+# #interating over list using range
+# for i in range(0, len(list_var)):
+#     print(list_var[i])
 
-#iterating over list directly using element
-print("**" * 30)
-for list_element in list_var:
-    print(list_element)
+# #iterating over list directly using element
+# print("**" * 30)
+# for list_element in list_var:
+#     print(list_element)
 
-#iterating over list using enumerate using both index and element
-print("**" * 30)
-for list_index, list_element in enumerate(list_var):
-    print(f"{list_element} is at {list_index}")
+# #iterating over list using enumerate using both index and element
+# print("**" * 30)
+# for list_index, list_element in enumerate(list_var):
+#     print(f"{list_element} is at {list_index}")
 
-print("**" * 30)
+# print("**" * 30)
 
-#Task 9
-num = [1,2,3,4,5,6,7,8,9,10]
-for even_num in num:
-    if even_num % 2 ==0:
-        print(even_num)
+# #Task 9
+# num = [1,2,3,4,5,6,7,8,9,10]
+# for even_num in num:
+#     if even_num % 2 ==0:
+#         print(even_num)
 
-print("**" *30)
+# print("**" *30)
 
-#How to add element in a list
-new_list = []
-print(new_list , len(new_list))
-
-# #staticly adding
-# new_list = [1]
+# #How to add element in a list
+# new_list = []
 # print(new_list , len(new_list))
 
-#dynamic adding 
-for i in range(1, 9):
-    new_list.append(i)
+# # #staticly adding
+# # new_list = [1]
+# # print(new_list , len(new_list))
+
+# #dynamic adding 
+# for i in range(1, 9):
+#     new_list.append(i)
     
-print(new_list , len(new_list))
+# print(new_list , len(new_list))
 
-print("**" *30)
+# print("**" *30)
 
-#Task 10
-even_list = []
-for num in range(1, 101):
-    if num%2 == 0 :
-        even_list.append(num)
+# #Task 10
+# even_list = []
+# for num in range(1, 101):
+#     if num%2 == 0 :
+#         even_list.append(num)
 
-print(even_list)
+# print(even_list)
+
+#slicing the list
+list_var = [1,2,3,4,5,6,7,8,9]
+print(list_var[1:]) #slices list from the 2nd element
+print(list_var[1:4]) #slices list from 2nd element to 5th element

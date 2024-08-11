7c6b7f27ee0604c0253f2a522bef51c7bde2585a
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-11 19:27:22 +0545

    task 9

--- Week 2 (Advanced Python)/Day 6 (Data Structures)/basics.py
+++ Week 2 (Advanced Python)/Day 6 (Data Structures)/basics.py
@@ -19,3 +19,11 @@ for list_index, list_element in enumerate(list_var):
     print(f"{list_element} is at {list_index}")
 
 print("**" * 30)
+
+#Task 9
+num = [1,2,3,4,5,6,7,8,9,10]
+for even_num in num:
+    if even_num % 2 ==0:
+        print(even_num)
+
+print("**" *30)

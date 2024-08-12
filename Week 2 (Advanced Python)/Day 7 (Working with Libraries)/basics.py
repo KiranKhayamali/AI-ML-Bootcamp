a5848461753880f6daa6711ae39888e8763300fe
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-12 19:59:52 +0545

    task 1

--- Week 2 (Advanced Python)/Day 7 (Working with Libraries)/basics.py
+++ Week 2 (Advanced Python)/Day 7 (Working with Libraries)/basics.py
@@ -27,3 +27,22 @@ print("After removing a column :")
 data_frame_var = data_frame_var.drop(columns= ['marks'])
 print(data_frame_var)
 
+#Task 1
+list_ques = []
+list_ans = []
+while True:
+    user_ques = input("Enter question: ")
+    user_ans = input("Enter answer of the question: ")
+    list_ques.append(user_ques)
+    list_ans.append(user_ans)
+    user_input = input("Do you want to continue? (Y/N): ")
+    if user_input.upper() == "N":
+        break
+
+# Create a DataFrame from the lists
+question_data_frame = pd.DataFrame({
+    'Question': list_ques,
+    'Answer': list_ans
+})
+
+print(question_data_frame)

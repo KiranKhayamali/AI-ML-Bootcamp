5aaee6603a17e90f0a77b6650526a2a987471504
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-13 19:06:30 +0545

    task 1

--- Week 2 (Advanced Python)/Day 8 (Error Handling and File Operations)/basics.py
+++ Week 2 (Advanced Python)/Day 8 (Error Handling and File Operations)/basics.py
@@ -8,10 +8,39 @@ Exception handling:
             if code fails rest of the part run here
 '''
 
-x = 0
-y = 10
-try:
-    print(f"the divison by 0 {y/x}")
-except Exception as e:
-    print(f"Got error as {e}!!!")
+# x = 0
+# y = 10
+# try:
+#     print(f"the divison by 0 {y/x}")
+# except Exception as e:
+#     print(f"Got error as {e}!!!")
 
+# import random
+# a = random.randint(0,100)
+# b = random.randint(0,100)
+# print(f"The random a {a} and {b} is divided as {b/a}")
+
+#task 1 
+# start infinite loop get random numerator and denomerator
+# and calculate total number of crashes 
+# if total number of crashes is greater than equal to 100 , program terminates
+
+#standard pratice 1 
+#try to do everything using function
+
+#standard pratice 2
+# Use try excpet on each function
+
+import random
+
+crashes =0
+while True:
+    numerator = random.randint(0,100)
+    denomerator = random.randint(0,100)
+    try:
+        output = numerator / denomerator
+    except Exception as e:
+        crashes +=1
+        print("crashes :", crashes)
+        if crashes >= 100:
+            break

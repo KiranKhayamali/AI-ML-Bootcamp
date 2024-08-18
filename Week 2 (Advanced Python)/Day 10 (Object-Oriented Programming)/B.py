8f9e3b451347e94530580357635170bdec5b5a8a
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-18 20:29:43 +0545

    introducing package and module

---
+++ Week 2 (Advanced Python)/Day 10 (Object-Oriented Programming)/B.py
@@ -0,0 +1,3 @@
+import A as obj_1
+
+print(f"Addition from B.py file : {obj_1.add(19, 23)}")
--- Week 2 (Advanced Python)/Day 10 (Object-Oriented Programming)/basics.py
+++ Week 2 (Advanced Python)/Day 10 (Object-Oriented Programming)/basics.py
@@ -72,23 +72,86 @@ Constructor:
 How does encapsulation and different instances works
 '''
 
-class Animal:
-    def __init__(self, animal_name, animal_species, animal_age):
-        self.name = animal_name
-        self.species = animal_species
-        self.age = animal_age
-    def display(self):
-        print(f"The animal is {self.name} having species of {self.species} with age {self.age}")
+# class Animal:
+#     def __init__(self, animal_name, animal_species, animal_age):
+#         self.name = animal_name
+#         self.species = animal_species
+#         self.age = animal_age
+#     def display(self):
+#         print(f"The animal is {self.name} having species of {self.species} with age {self.age}")
 
-dog_animal = Animal('bob', 'german shephard', 5)
-cat = Animal ('kuro', 'black panther', 9)
+# dog_animal = Animal('bob', 'german shephard', 5)
+# cat = Animal ('kuro', 'black panther', 9)
 
-print("Accessing information from outside of the class:")
-print(dog_animal.name, dog_animal.species, dog_animal.age)
-print(cat.name, cat.species, cat.age)
+# print("Accessing information from outside of the class:")
+# print(dog_animal.name, dog_animal.species, dog_animal.age)
+# print(cat.name, cat.species, cat.age)
 
-print("Accessing the information from inside the class:")
-dog_animal.display()
-cat.display()
+# print("Accessing the information from inside the class:")
+# dog_animal.display()
+# cat.display()
 
 
+
+'''
+Task 2: 
+    WAP that uses oop
+    has class user
+    when initalized it provides options: 
+    1. Sign up 
+    2. Sign in 
+
+    if 1 is pressed it redirects to a method named Register_User
+    this method takes in username, password and mobile number
+
+    if 2 is pressed it redirects to a method name authentication
+    this method checks input username and password with self.username
+    and self.password 
+
+    if match it redirects to another method named welcome_user
+
+    welcome user method greets them,displays username and displays 
+    their mobile number
+
+    also this method gives 3 options. 
+    1. enter note 
+    2. Display notes
+    3. exit 
+
+    if 1 is pressed user can input a large note that is saved for that 
+    object 
+
+    if 2 is pressed display the note that was saved earlier 
+
+    if 3 is pressed terminate 
+'''
+class User:
+    def __init__(self):
+        self.file = 'users.json'
+        self.notes = []
+        self.load_user()
+
+    def welcome_user(self):
+        print(f"Hello {self.username} to the program! \n {self.username} has mobile number : {self.number}")
+        user_input = input("1. Enter Notes \n2. Display Notes \n3. Exit \n:")
+
+    def register_user(self):
+        self.username = input("Enter UserName :")
+        self.password = input("Register a password :")
+        self.number = input(f"Hey {self.username}! What is your mobile number :")
+
+    def authenticate(self):
+        username = input("Enter your username :")
+        password = input("Enter your password :")
+        if username == self.username and password == self.password:
+            self.welcome_user()
+
+    user_input = input(f"1. Sign up \n2.Sign up \n3.Exit \n:")
+    if user_input == 1:
+        register_user()
+    elif user_input == 2:
+        authenticate()
+    elif user_input == 3:
+        exit
+
+user1 = User()
